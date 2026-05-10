eb0b3cdde938a9163115327d6d373fb122f24893
Bozhang Zhou <bozhangz@uci.edu>
2026-05-10 16:21:45 -0700

    Initial commit for assignment 2

---
+++ a2.py
@@ -0,0 +1,9 @@
+# a2.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME Bozhang Zhou
+# EMAIL bozhangz@uci.edu
+# STUDENT ID 93213406
---
+++ ui.py
@@ -0,0 +1,9 @@
+# ui.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME Bozhang Zhou
+# EMAIL bozhangz@uci.edu
+# STUDENT ID 93213406
